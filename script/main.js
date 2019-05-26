d2c9c30869eb862d27d42399a0d0f81e09e28758
Doggoduck <34210285+kipkat@users.noreply.github.com>
2019-05-26 17:10:45 +0300

    Modify credits and header text feature

--- script/main.js
+++ script/main.js
@@ -1,6 +1,20 @@
 function addFullscreenButton()
 {
 	document.getElementsByClassName("dweet-actions")[0].innerHTML
-  += `<a style="border: 0px; margin: 0px; margin-left: 10px; padding: 0px;" class="dweet-option fullscreen-button" href="javascript:;" target="_blank">fullscreen</a>`
+  += `<a style="border: 0px; margin: 0px; margin-left: 10px; padding: 0px;" class="dweet-option fullscreen-button" href="javascript:;" target="_blank">fullscreen</a>`;
 }
-addFullscreenButton()
+
+function changeHeaderText()
+{
+	document.getElementById("dwitter-header-text").innerHTML += " +";
+}
+
+function addCredits()
+{
+	document.getElementById("settings-list").innerHTML += "<li><a style='color: red;' href='kipkat'>Dwitter +</a></li>"
+}
+
+// execution
+addFullscreenButton();
+changeHeaderText();
+addCredits();
